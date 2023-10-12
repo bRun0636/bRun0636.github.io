92aa7da60ba0b30e3c2bb3871ef080491bc76f81
bRun0 <mem.22@uni-dubna.ru>
2023-10-12 15:44:44 +0300

    js

--- site/scripts/script.js
+++ site/scripts/script.js
@@ -1,3 +1,21 @@
-function btn(){
-    alert("Привет");
+function check(){
+    console.log("Здравствуйте " + document.forms[0].name.value)
+    for(var i = 0; i<3; i++){
+        if(document.forms[0].choose[i].checked){
+            console.log("Ответ будет " + document.forms[0].choose[i].value);
+        } 
+    }
+  
+    
+    console.log("Вами были выбраны языки: ")
+    for(var i = 0; i<3; i++){
+        if(document.forms[0].chckbx[i].checked)
+        console.log(document.forms[0].chckbx[i].value);
+    }
+
+    console.log("Цель вашего обращения " + document.forms[0].theme[document.forms[0].theme.selectedIndex].text);
+    console.log("Ваш номер: " + document.forms[0].tel.value)
+    console.log("Ваша почта: " + document.forms[0].email.value)
+    console.log("Ваше обращение: " + document.forms[0].question.value)
+
 }
